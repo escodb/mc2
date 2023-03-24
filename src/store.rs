accc1e9c43c5b4e3ed03680df1849f1a5564ab65
James Coglan <jcoglan@gmail.com>
2023-03-24 18:46:20 +0000

    Implement Cache, which models a client's memory of values it read from the Store

--- src/store.rs
+++ src/store.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::collections::BTreeMap;
 
 type Rev = usize;
@@ -47,6 +48,57 @@ where
 
     pub fn keys(&self) -> Vec<&str> {
         self.data.keys().map(|key| key.as_ref()).collect()
+    }
+}
+
+pub struct Cache<'a, T> {
+    store: &'a RefCell<Store<T>>,
+    data: BTreeMap<String, Option<(Rev, T)>>,
+}
+
+impl<T> Cache<'_, T>
+where
+    T: Clone,
+{
+    pub fn new(store: &RefCell<Store<T>>) -> Cache<T> {
+        Cache {
+            store,
+            data: BTreeMap::new(),
+        }
+    }
+
+    pub fn read(&mut self, key: &str) -> Option<T> {
+        if !self.data.contains_key(key) {
+            let record = self.store.borrow().read(key);
+            self.data.insert(key.into(), record);
+        }
+
+        if let Some(Some((_, value))) = self.data.get(key) {
+            Some(value.clone())
+        } else {
+            None
+        }
+    }
+
+    pub fn write(&mut self, key: &str, value: T) -> bool {
+        let old_rev = self.get_rev(key);
+        let mut store = self.store.borrow_mut();
+
+        if let Some(new_rev) = store.write(key, old_rev, value.clone()) {
+            self.data.insert(key.into(), Some((new_rev, value)));
+            true
+        } else {
+            self.data.remove(key);
+            false
+        }
+    }
+
+    fn get_rev(&self, key: &str) -> Option<Rev> {
+        if let Some(Some((rev, _))) = self.data.get(key) {
+            Some(*rev)
+        } else {
+            None
+        }
     }
 }
 
@@ -108,5 +160,98 @@ mod tests {
         store.write("/z/doc.json", None, 53);
 
         assert_eq!(store.keys(), vec!["/", "/path/", "/z/doc.json"]);
+    }
+
+    #[test]
+    fn returns_none_for_an_unknown_key() {
+        let store: RefCell<Store<()>> = RefCell::new(Store::new());
+        let mut cache = Cache::new(&store);
+
+        assert_eq!(cache.read("x"), None);
+    }
+
+    #[test]
+    fn reads_a_value_from_the_store() {
+        let store = RefCell::new(Store::new());
+        let mut cache = Cache::new(&store);
+
+        assert_eq!(store.borrow_mut().write("x", None, 51), Some(1));
+        assert_eq!(cache.read("x"), Some(51));
+    }
+
+    #[test]
+    fn caches_a_read_that_returns_none() {
+        let store = RefCell::new(Store::new());
+        let mut cache = Cache::new(&store);
+
+        assert_eq!(cache.read("x"), None);
+        assert_eq!(store.borrow_mut().write("x", None, 51), Some(1));
+        assert_eq!(cache.read("x"), None);
+    }
+
+    #[test]
+    fn writes_a_value_to_the_store() {
+        let store = RefCell::new(Store::new());
+        let mut cache = Cache::new(&store);
+
+        assert_eq!(cache.write("x", 51), true);
+
+        assert_eq!(store.borrow().read("x"), Some((1, 51)));
+        assert_eq!(cache.read("x"), Some(51));
+    }
+
+    #[test]
+    fn updates_a_value_in_the_store() {
+        let store = RefCell::new(Store::new());
+        let mut cache = Cache::new(&store);
+
+        assert_eq!(cache.write("x", 51), true);
+        assert_eq!(cache.write("x", 52), true);
+        assert_eq!(cache.write("x", 53), true);
+
+        assert_eq!(store.borrow().read("x"), Some((3, 53)));
+        assert_eq!(cache.read("x"), Some(53));
+    }
+
+    #[test]
+    fn fails_to_update_a_doc_it_did_not_read_first() {
+        let store = RefCell::new(Store::new());
+        let mut cache = Cache::new(&store);
+
+        assert_eq!(store.borrow_mut().write("x", None, 51), Some(1));
+        assert_eq!(cache.write("x", 52), false);
+
+        assert_eq!(store.borrow().read("x"), Some((1, 51)));
+        assert_eq!(cache.read("x"), Some(51));
+    }
+
+    #[test]
+    fn fails_to_update_with_a_stale_read() {
+        let store = RefCell::new(Store::new());
+        let mut cache = Cache::new(&store);
+
+        assert_eq!(cache.write("x", 51), true);
+
+        assert_eq!(store.borrow_mut().write("x", Some(1), 53), Some(2));
+        assert_eq!(cache.write("x", 52), false);
+
+        assert_eq!(store.borrow().read("x"), Some((2, 53)));
+    }
+
+    #[test]
+    fn recovers_after_a_failed_write() {
+        let store = RefCell::new(Store::new());
+        let mut cache = Cache::new(&store);
+
+        assert_eq!(cache.write("x", 51), true);
+
+        assert_eq!(store.borrow_mut().write("x", Some(1), 53), Some(2));
+        assert_eq!(cache.write("x", 52), false);
+
+        assert_eq!(cache.read("x"), Some(53));
+        assert_eq!(cache.write("x", 52), true);
+
+        assert_eq!(store.borrow().read("x"), Some((3, 52)));
+        assert_eq!(cache.read("x"), Some(52));
     }
 }
